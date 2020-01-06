21225147e0faf56c7e822ead0fbfc15c32dfd64c
srisairam_gaarlapati <srisairam_gaarlapati@f217b49b-f41f-0410-bcdc-e4b5239f133b>
2020-01-06 09:38:16 +0000

    ChargeBootstarpCapacitors function is enabled
    git-svn-id: https://chn-vm-apps.microchip.com/mchprepo/MCU16/applications/motor_control/MC_Projects/AN1292_HURST/branches/AN1299_dsPIC33CK256MP508_EXT_INT_OPAMP_LVMC@97589 f217b49b-f41f-0410-bcdc-e4b5239f133b

--- hal/pwm.c
+++ hal/pwm.c
@@ -160,7 +160,7 @@ void InitPWMGenerators(void)
     PG1CONLbits.ON = 1;      // Enable PWM module after initializing generators
 
     /* Function call to charge Bootstrap capacitors*/
-//    ChargeBootstarpCapacitors();
+    ChargeBootstarpCapacitors();
 }
 // *****************************************************************************
 /* Function:
